dc3ce1a5f703620321affc9d415af4caab6897b8
Tetro <itsofirk@gmail.com>
2020-07-01 20:37:15 +0300

    Done ---------------- reading from the second kafka topic performing word count printing to console

---
+++ top10_words.py
@@ -0,0 +1,35 @@
+from os import path
+import pyspark.sql.functions as F
+
+import configs
+import constants as C
+import utils
+
+APPNAME = 'naya-twitter-aggregation'
+
+
+def write_to_console():
+    ds = df.writeStream \
+        .option("checkpointLocation", path.join(C.CHECKPOINT_LOCATION, APPNAME)) \
+        .format("console") \
+        .outputMode("complete") \
+        .start()
+    ds.processAllAvailable()
+
+
+if __name__ == '__main__':
+    spark = utils.get_spark_session(APPNAME)
+    spark.builder.config("spark.sql.shuffle.partitions", "8")
+
+    df = utils.read_from_kafka(spark, configs.get_kafka_host(), configs.get_words_topic())
+
+    df = df.selectExpr("CAST(value AS STRING)") \
+        .withColumnRenamed('value', 'word')
+
+    # Word count
+    df = df.groupBy('word') \
+        .count() \
+        .orderBy(F.desc('count')) \
+        .limit(10)
+
+    write_to_console()
